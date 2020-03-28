5e5c734a9dc027b8458b19edbbb7a0113114bf3a
mde-pach <>
2020-03-28 20:47:14 +0100

    change address of backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://condottiere-core.herokuapp.com/"
   },
   /*
   ** Build configuration
--- plugins/websocket.js
+++ plugins/websocket.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 
-Vue.use(VueNativeSock, 'ws://127.0.0.1:8000/ws')
+Vue.use(VueNativeSock, 'ws://condottiere-core.herokuapp.com/ws')
